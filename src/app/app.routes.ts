fcb6948d58debc8470ae0ea072b0544308f90baa
pablotsky321 <jprestrepo346@gmail.com>
2024-03-17 21:09:29 -0500

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { NotesComponent } from './pages/notes/notes.component';
+import { AboutComponent } from './pages/about/about.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'notes',
+        title:'NOTES',
+        component:NotesComponent
+    },
+    {
+        path:'about',
+        title:'ABOUT',
+        component:AboutComponent
+    },
+    {
+        path:'login',
+        title:'LOGIN',
+        component:LoginComponent
+    },
+    {
+        path:'register',
+        title:'REGISTER',
+        component:RegisterComponent
+    },
+    {
+        path:'',
+        redirectTo:'/notes',
+        pathMatch:'full'
+    }
+];
